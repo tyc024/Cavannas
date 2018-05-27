a4fbd50d5cb47df6df08bdb7128f52a067c70ed7
Jessica Wu <ycw003@ucsd.edu>
2018-05-26 22:42:23 -0700

    add index.html, index.css, and connect them to React App

--- react-client/react-app/server.js
+++ react-client/react-app/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+
+/*
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+*/
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
